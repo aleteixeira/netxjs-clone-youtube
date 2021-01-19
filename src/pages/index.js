25250cb51f491b72d7d9a66f4be6fe41685f0c2b
unknown <alexandre@caracolnet.com.br>
2021-01-18 21:31:06 -0300

    Material-UI configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
-  return <div>Clone youtube com Nextjs + Material-UI</div>;
+  return (
+    <div>
+      Clone youtube com Nextjs + Material-UI
+      <Button variant="outlined" color="secondary">
+        Hello World
+      </Button>
+      ;
+    </div>
+  );
 }
